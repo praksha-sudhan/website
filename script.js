8ad7984cd8695e9e0c50d145756097b8be5b73d4
praksha-sudhan <prakshasudhan6@gmail.com>
2025-03-02 09:10:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add interactivity to buttons if needed
+document.querySelectorAll('.buy-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        alert("This product has been added to your cart.");
+    });
+});
